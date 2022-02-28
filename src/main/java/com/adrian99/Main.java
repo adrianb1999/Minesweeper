e1af5b85facff6d457c1ac73b2af4628e0691b15
adrianb1999 <blanariu_adrian@yahoo.com>
2022-02-28 09:08:15 +0200

    Maven added

--- src/main/java/com/adrian99/Main.java
+++ src/main/java/com/adrian99/Main.java
@@ -6,7 +6,6 @@ import com.adrian99.gui.MainMenu;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Current directory = " + System.getProperty("user.dir"));
         GameManager gameManager = new GameManager();
         new MainMenu(gameManager).showMainMenu();
     }
